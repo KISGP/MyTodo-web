e7aeaacbf2ca87a8dbd7c59fa3e93f4ef030f733
kis <2330029579@qq.com>
2024-09-01 22:17:40 +0800

    fix: lexical工具栏无法显示正确显示当前段落类型

--- src/components/ui/editor/plugins/ToolbarPlugin.tsx
+++ src/components/ui/editor/plugins/ToolbarPlugin.tsx
@@ -1,7 +1,10 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-
-import { mergeRegister } from "@lexical/utils";
 import { Divider } from "@nextui-org/react";
+import { $isListNode, ListNode } from "@lexical/list";
+import { $isHeadingNode } from "@lexical/rich-text";
+import { $isLinkNode, TOGGLE_LINK_COMMAND } from "@lexical/link";
+import { mergeRegister, $getNearestNodeOfType } from "@lexical/utils";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import {
   $getSelection,
   $isRangeSelection,
@@ -14,8 +17,6 @@ import {
   UNDO_COMMAND,
   COMMAND_PRIORITY_CRITICAL,
 } from "lexical";
-import { $isLinkNode, TOGGLE_LINK_COMMAND } from "@lexical/link";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
 import UndoButton from "../ui/undo-button.tsx";
 import RedoButton from "../ui/redo-button.tsx";
@@ -48,6 +49,26 @@ export default function ToolbarPlugin() {
   const $updateToolbar = useCallback(() => {
     const selection = $getSelection();
     if ($isRangeSelection(selection)) {
+      const anchorNode = selection.anchor.getNode();
+      const element =
+        anchorNode.getKey() === "root"
+          ? anchorNode
+          : anchorNode.getTopLevelElementOrThrow();
+      const elementKey = element.getKey();
+      const elementDOM = editor.getElementByKey(elementKey);
+      if (elementDOM !== null) {
+        if ($isListNode(element)) {
+          const parentList = $getNearestNodeOfType(anchorNode, ListNode);
+          const type = parentList ? parentList.getTag() : element.getTag();
+          setBlockType(type);
+        } else {
+          const type = $isHeadingNode(element)
+            ? element.getTag()
+            : element.getType();
+          setBlockType(type);
+        }
+      }
+
       // Update text format
       setIsBold(selection.hasFormat("bold"));
       setIsCode(selection.hasFormat("code"));
